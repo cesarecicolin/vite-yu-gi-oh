38abb23265ca0762d176180765ba2565c00a4e94
cesarecicolin <cece.caster@gmail.com>
2023-11-27 17:44:02 +0100

    Add: aggiunta la lista di carte

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script >
-
+import axios from "axios"
+import {store} from "./store.js"
+export default{
+  data(){
+    return{
+      store,
+    }
+  },
+  created(){
+   axios.get(this.store.apiUrl)
+   .then((resp) =>{
+    this.store.cardList = resp.data
+   })   
+  }
+}
 </script>
 
 <template>
- <h1></h1>
+ <h1>ciao</h1>
 </template>
 
 <style lang="scss">
-@use "./style/general.scss"
+@use "./style/general.scss";
+
 </style>
