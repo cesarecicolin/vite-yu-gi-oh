ec69037d49c59eab042a69aac42caf565497b3d9
cesarecicolin <cece.caster@gmail.com>
2023-11-28 16:04:05 +0100

    add ricerca delle carte per archetipo

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import axios from "axios"
 import {store} from "./store.js"
 import Appheader from "./components/Appheader.vue"
 import AppMain from "./components/AppMain.vue"
+import Appselect from "./components/Appselect.vue"
 export default{
     data() {
         return {
@@ -16,12 +17,28 @@ export default{
             console.log(store.cardList);
         });
     },
-    components: { Appheader, AppMain }
+    components: { Appheader, AppMain, Appselect },
+    methods:{
+        cerca(){
+            console.log("cerca");
+            axios
+            .get(this.store.apiUrl,{
+                params: {
+                    archetype: this.store.scelta,
+                },
+            })
+            .then((resp) => {
+                this.store.cardList = resp.data.data
+            })
+        }
+
+    }
 }
 </script>
 
 <template>
  <Appheader />
+ <Appselect @search="cerca" />
  <AppMain />
 </template>
 
---
+++ src/components/Appselect.vue
@@ -0,0 +1,29 @@
+<script>
+import { store } from '../store';
+export default {
+    data(){
+      return{
+        store
+      }
+    }
+}
+</script>
+
+<template>
+<div class="d-flex mb-3 ">
+    <select class="form-select" v-model="this.store.scelta" >
+  <option selected>Open this select menu</option>
+  <option value="Alien"> Alien</option>
+  <option value="Ally of Justice">Ally of Justice</option>
+  <option value="Ancient Gear">Ancient Gear</option>
+</select>
+<button class="btn btn-primary" @click="$emit('search')">Cerca</button>
+</div>
+
+</template>
+
+<style lang="scss" scoped>
+select{
+  width: 300px;
+}
+</style>
